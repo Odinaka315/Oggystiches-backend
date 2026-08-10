a5e1b61ccedb3b5cf09f7409a2306be5f8243c81
Odinaka315 <nwolisaodinaka5@gmail.com>
2026-08-10 10:27:33 +0100

    changed .env configuration

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -22,15 +22,11 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
-    redis_url: str = "redis://localhost:6379/0"
     cloudinary_name: str
     cloudinary_api_key: str
     cloudinary_api_secret: str
     cloudinary_secure: bool
-    gmail_app_password: str
-    paystack_secret_key: str
     brevo_api_key: str
-    google_client_id: str
 
     class Config:
         env_file = ".env"
